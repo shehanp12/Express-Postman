66eba35bc15280dab073f4ba0e741d1124cb7e17
shehanp12 <shehanp068@gmail.com>
2020-12-02 16:34:57 +0530

    memebers json object

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const  express = require('express');
-
+const path = require('path');
 const app = express();
 
-app.get('/',(req,res) =>{
-    res.send("<h1>Hello World</h1>")
+const members = [
+    {
+        id:1,
+        name:"Shehan Prasanna",
+        email:"shehanp068@gmail.com",
+        status:"active"
+    },
+    {
+        id:2,
+        name:"Charith Eranga",
+        email:"charith068@gmail.com",
+        status:"active"
+    },
+    {
+        id:3,
+        name:"Hashan Danapla",
+        email:"hashan@gmail.com",
+        status:"offline"
+    },
+];
+
+app.get('/api/members',(req,res) =>{
+ res.json(members);
 });
+//Set static folder
+app.use(express.static(path.join(__dirname,'public')));
 
 const PORT = process.env.PORT || 5000;
 
