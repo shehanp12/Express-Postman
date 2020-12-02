0637033ce2ef4fbefd7af8b3fa85096a54c398ce
shehanp12 <shehanp068@gmail.com>
2020-12-02 17:22:41 +0530

    app js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const  express = require('express');
 const path = require('path');
 const app = express();
 const members = require('./Members')
-
+const moment = require('moment')
 const logger = (req,res,next ) => {
-    console.log("Hello ");
+    console.log(`${req.protocol} ://${req.get('host')}${req.originalUrl}: ${moment().format()}`);
     next();
 
 
@@ -17,6 +17,23 @@ app.use(logger);
 app.get('/api/members',(req,res) =>{
  res.json(members);
 });
+
+//Get Single Member
+app.get('/api/members/:id',(req,res)=>{
+ const found = members.some(member => member.id === parseInt(req.params.id) )
+
+    if(found){
+        res.json(members.filter(member => member.id === parseInt(req.params.id)))
+    }
+    else{
+        res.status(400).json({msg:`No members with the id  of ${req.params.id}`})
+    }
+});
+
+
+
+
+
 //Set static folder
 app.use(express.static(path.join(__dirname,'public')));
 
